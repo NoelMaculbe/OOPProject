d922011480939e2f857d9a45f15104b7f53c0bbf
NoelMaculbe <45276402+NoelMaculbe@users.noreply.github.com>
2018-11-23 09:42:13 +0800

    Added Files
    Added Person.cs, Product.cs, Calculations.cs

---
+++ Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_project
+{
+    class Person
+    {
+        string FirstName;
+        string MiddleInitial;
+        string LastName;
+        string BirthDate;
+        string Address;
+
+        public string GetFullName()
+        {
+            return null;
+        }
+
+        public int GetAge()
+        {
+            return 0;
+
+        }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_project
+{
+    class Product
+    {
+        string Name;
+        string Description;
+        string Price;
+        string Manufacturer;
+        int Items;
+
+        public void DeductItems(int items)
+        {
+
+        }
+    }
+}
